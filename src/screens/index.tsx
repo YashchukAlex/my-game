5cc444c499645bcee354cfe6b22cf08e6a791904
Oleksii <oleksii@jadaapp.com>
2022-12-13 15:04:41 +0200

    add game refresh

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,17 +1,28 @@
+//libraries
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
 } from 'react-native-reanimated';
 
+//components
 import Floor from '../components/floor/index';
 import Car from '../components/car';
 import ToolControlSpeed from '../components/toolControlSpeed';
 
+//utils
 import { Car as CarType, Wheel } from '../engine/entities';
 import { loop } from '../engine';
 import { frameDelay } from '../constants';
+
+//assets
+import RefreshSVG from '../assets/refresh';
 
 export default () => {
   const [carSpeed, setCarSpeed] = useState<number>(0);
@@ -40,8 +51,19 @@ export default () => {
     }
   }, [carSpeed, mapMoving, carMoving]);
 
+  const refreshGame = () => {
+    mapMoving.value = 0;
+    const newCarValue: CarType = JSON.parse(JSON.stringify(carMoving.value));
+    newCarValue.position.x = 0;
+    carMoving.value = newCarValue;
+    setCarSpeed(0);
+  };
+
   return (
     <>
+      <TouchableOpacity onPress={refreshGame} style={styles.refreshButton}>
+        <RefreshSVG width={35} height={35} />
+      </TouchableOpacity>
       <ToolControlSpeed changeSpeed={setCarSpeed} />
       <Animated.View style={[styles.container, animatedStyles]}>
         <Car carMoving={carMoving} />
@@ -54,5 +76,11 @@ export default () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  refreshButton: {
+    position: 'absolute',
+    left: 30,
+    top: 30,
+    zIndex: 999,
   },
 });
